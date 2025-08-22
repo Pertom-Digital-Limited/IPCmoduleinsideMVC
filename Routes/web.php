ac88db519811dbb584aa2f28604296bc4041061b
Peter Muturi Wairimu <65893854+muturipeter@users.noreply.github.com>
2025-08-22 13:24:18 +0300

    Renamed and redid routes so that they aren't hardcoded

---
+++ Routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+
+return [
+    '' => 'AthleteController@index',            // List all athletes
+    'athlete/create' => 'AthleteController@create', // Show registration form
+    'athlete/store'  => 'AthleteController@store',  // Handle form submission
+];
